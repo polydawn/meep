809a3a8671cc5a4e4741f70f48cc433dd2d8b9be
Eric Myhre <hash@exultant.us>
2016-04-30 13:39:31 -0500

    Build out example|test of TraceableError.
    The hardest part here continues to be hacking around the absolute paths that crop up in strings.  I'd really vastly prefer to reference full package paths, independently of the filesystem and totally disregarding the $GOPATH... but that information simply doesn't seem to be around, unfortunately.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- traceable_test.go
+++ traceable_test.go
@@ -2,6 +2,7 @@ package meep_test
 
 import (
 	"fmt"
+	"strings"
 
 	"."
 )
@@ -12,8 +13,24 @@ func ExampleTraceableErr() {
 		error
 	}
 	err := meep.New(&Woop{})
-	fmt.Println(err.(*Woop).StackString())
+	str := err.(*Woop).StackString()
 
-	/// Output:
-	// FIXME this is hard to test because of the full local path that pops up :(
+	// The *entire* output probably looks something like this:
+	//		·> /your/build/path/meep/meep.go:8: meep.New
+	//		·> /your/build/path/meep/traceable_test.go:15: meep_test.ExampleTraceableErr
+	//		·> /usr/local/go/src/testing/example.go:98: testing.runExample
+	//		·> /usr/local/go/src/testing/example.go:36: testing.RunExamplesa
+	//		·> /usr/local/go/src/testing/testing.go:486: testing.(*M).Run
+	//		·> _/your/build/path/meep/_test/_testmain.go:64: main.main
+	//		·> /usr/local/go/src/runtime/proc.go:63: runtime.main
+	//		·> /usr/local/go/src/runtime/asm_amd64.s:2232: runtime.goexit
+	// We filter it down rather dramatically so as not to catch any line
+	//  numbers from the stdlib we built against, etc.
+
+	str = strings.Split(str, "\n")[1]      // yank the one interesting line
+	str = strings.Replace(str, cwd, "", 1) // strip the local build path
+	fmt.Println(str)
+
+	// Output:
+	//	·> /traceable_test.go:15: meep_test.ExampleTraceableErr
 }
