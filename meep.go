c4551c0dc3fd64071a514075be8ea2dd3ba7fb1c
Eric Myhre <hash@exultant.us>
2016-05-09 19:42:49 -0500

    Comments in New just to remind self what's covered and what's actually not requiring of a special mention.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- meep.go
+++ meep.go
@@ -1,11 +1,18 @@
 package meep
 
 func New(err error) error {
+	// for `AutodescribingError`:
 	if m, ok := err.(meepAutodescriber); ok {
 		m.isMeepAutodescriber().self = err
 	}
+
+	// for `TraceableError`:
 	if m, ok := err.(meepTraceable); ok {
 		m.isMeepTraceable().Stack = *captureStack()
 	}
+
+	// for `CauseableError`:
+	//  (nothing really; it's mostly a hint to `AutodescribingError`.)
+
 	return err
 }
