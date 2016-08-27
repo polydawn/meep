518da9f8ca710604ad4b1cd5a09b7ef954606f23
Eric Myhre <hash@exultant.us>
2016-08-27 12:31:26 -0500

    Split apart the TryPlan and the actual Try().
    Combining them the first time was kind of a mistake.  It made it hard to construct plans outside and reuse them outside of their use site.  This should now be easier to do.
    
    We're also free to construct plans and then have more different mechanisms for *applying* them as a separate issue, which seems wise.
    
    The default path with the `Try()` function should retain the same overall ordering as before though (code first, handlers follow).
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- try.go
+++ try.go
@@ -1,57 +1,51 @@
 package meep
 
-import (
-	"reflect"
-)
-
-/*
-	TryPlan is an error handling configuration.
-
-	You can dispatch errors according to several patterns, since both the
-	golang stdlib and many libraries have seen fit to use a variety of
-	different patterns, and they can't easily be distinguished by type alone
-	(e.g. should we typeswitch, or do we need to do actual val/ptr compare):
-
-		Catch(kind error, handler)
-		CatchVal(ptrOrVal error, handler)
-		CatchPredicate(func(error) bool, handler) // use only as a last resort, please
-		CatchExotic(handler) // anyone who panics as a non error goes here.  nobody should do that, frankly.
-		CatchAll(handler) // called as a last resort
-*/
-type TryPlan struct {
-	main     func()
-	main2    func() error // we could do this with yet more wrappers but it's preferable to keep stacks shorter where possible
-	matchers []tryMatcher
-	finally  func()
+func Try(fn func(), plan TryPlan) {
+	defer func() {
+		if err := coerce(recover()); err != nil {
+			handler := match(err, plan)
+			if handler == nil {
+				panic(err)
+			}
+			handler(err)
+		}
+	}()
+	fn()
 }
 
-type TryHandler func(error)
-
-/*
-	Accepts a function, which will be run with `recover` wrapping, and any
-	panics routed according to the `Catch*`'s set up.
-*/
-func Try(f func()) *TryPlan {
-	return &TryPlan{main: f, finally: func() {}}
-}
-
-func Try2(f func() error) *TryPlan {
-	return &TryPlan{main2: f, finally: func() {}}
-}
-
-func (p *TryPlan) Catch(kind error, handler TryHandler) *TryPlan {
-	p.matchers = append(p.matchers, tryMatcher{
-		predicate: tryPredicateType{reflect.TypeOf(kind)}.Q,
-		handler:   handler,
-	})
-	return p
-}
-
-func (p *TryPlan) Finally(f func()) *TryPlan {
-	f2 := p.finally
-	p.finally = func() {
-		f()
-		f2()
+func coerce(rcvrd interface{}) error {
+	switch err := rcvrd.(type) {
+	case nil:
+		// Panics of nils are possible btw but super absurd.  Never do it.
+		return nil
+	case error:
+		return err
+	default:
+		// Panics of non-error types are bad and you should feel bad.
+		return New(&ErrUntypedPanic{Cause: rcvrd})
 	}
-	return p
+}
+
+/*
+	Runs the `rcvrd` value through the whole `plan`, and
+	returns the first TryHandler the plan matches, or nil if no matches.
+*/
+func match(err error, plan TryPlan) TryHandler {
+	for _, matcher := range plan.matchers {
+		if matcher.predicate(err) {
+			return matcher.handler
+		}
+	}
+	return nil
+}
+
+/*
+	A wrapper for non-error types raised from a panic.
+
+	The `Try` system will coerce all non-error types to this automatically.
+*/
+type ErrUntypedPanic struct {
+	AutodescribingError
+	TraceableError
+	Cause interface{}
 }
---
+++ tryplan.go
@@ -0,0 +1,37 @@
+package meep
+
+import (
+	"reflect"
+)
+
+/*
+	TryPlan is an error handling configuration.
+
+	You can dispatch errors according to several patterns, since both the
+	golang stdlib and many libraries have seen fit to use a variety of
+	different patterns, and they can't easily be distinguished by type alone
+	(e.g. should we typeswitch, or do we need to do actual val/ptr compare):
+
+		Catch(kind error, handler)
+		CatchVal(ptrOrVal error, handler)
+		CatchPredicate(func(error) bool, handler) // use only as a last resort, please
+		CatchExotic(handler) // anyone who panics as a non error goes here.  nobody should do that, frankly.
+		CatchAll(handler) // called as a last resort
+
+	If an error passes all the way through a TryPlan without matching any of
+	the configured handlers, it is raised as a panic.
+	If you want to catch *everything*, use the CatchAll handler.
+*/
+type TryPlan struct {
+	matchers []tryMatcher
+}
+
+type TryHandler func(error)
+
+func (p TryPlan) Catch(typeExample error, handler TryHandler) TryPlan {
+	p.matchers = append(p.matchers, tryMatcher{
+		predicate: tryPredicateType{reflect.TypeOf(typeExample)}.Q,
+		handler:   handler,
+	})
+	return p
+}
