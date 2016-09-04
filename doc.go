93281135d993fdc6f8d1fa5f3b9e5d0cb9ceb018
Eric Myhre <hash@exultant.us>
2016-09-04 18:15:19 -0500

    Drop a doc string that's rather stale.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- doc.go
+++ doc.go
@@ -6,16 +6,3 @@
 	declaring useful error types is now *much* easier.
 */
 package meep
-
-/*
-	Implementation pattern notes:
-
-	All the public stuff is made available by embedding it into a struct
-	of the user-library's definition.
-	These are all the in `mixins.go` file.
-
-	Many of the mixins implement an interface which is private to this package
-	and simply returns a mutable reference to themselves.
-	This is so we can do type switching against that interface (instead
-	needing reflection), and then use it to reach in and initialize the value.
-*/
