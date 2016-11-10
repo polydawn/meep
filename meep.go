378778441583c94acb03d78442f4a6982e87a919
Eric Myhre <hash@exultant.us>
2016-11-10 15:19:22 -0800

    Add a new public option to skip capturing stacks when calling `Meep` to raise a new error.
    The default is by far to always capture stacks for an error with the Traceable trait.  But exceptions exist (so far, the only defensible ones are where an error type is sometimes recursive in its own cause, and only the first stack matters), so, here's support.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- meep.go
+++ meep.go
@@ -5,6 +5,8 @@ func Meep(err error, opts ...Opts) error {
 }
 
 func New(err error, opts ...Opts) error {
+	opt := flattenOpts(opts)
+
 	// for `TraitAutodescribing`:
 	if m, ok := err.(meepAutodescriber); ok {
 		m.isMeepAutodescriber().self = err
@@ -12,16 +14,15 @@ func New(err error, opts ...Opts) error {
 
 	// for `TraitTraceable`:
 	if m, ok := err.(meepTraceable); ok {
-		m.isMeepTraceable().Stack = *captureStack()
+		if !opt.nostack {
+			m.isMeepTraceable().Stack = *captureStack()
+		}
 	}
 
 	// for `TraitCausable`:
 	if m, ok := err.(meepCausable); ok {
-		for _, o := range opts {
-			if o.cause != nil {
-				m.isMeepCausable().Cause = o.cause
-				break
-			}
+		if opt.cause != nil {
+			m.isMeepCausable().Cause = opt.cause
 		}
 	}
 
@@ -29,9 +30,29 @@ func New(err error, opts ...Opts) error {
 }
 
 type Opts struct {
-	cause error
+	cause   error
+	nostack bool
+}
+
+func flattenOpts(opts []Opts) Opts {
+	v := Opts{}
+	for _, o := range opts {
+		if o.cause != nil {
+			v.cause = o.cause
+		}
+		if o.nostack == true {
+			v.nostack = true
+		}
+
+	}
+	return v
+
 }
 
 func Cause(x error) Opts {
 	return Opts{cause: New(x)}
+}
+
+func NoStack() Opts {
+	return Opts{nostack: true}
 }
